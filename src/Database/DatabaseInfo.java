0c4ce74396c2c0e47b60e54dc81297445cb768b2
dangnghiemhong <dangnghiem200@gmail.com>
2022-06-16 10:16:00 +0700

    SQL

---
+++ src/Database/DatabaseInfo.java
@@ -0,0 +1,10 @@
+package Database;
+
+public class DatabaseInfo {
+    //public static String driverName = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    //public static String dbURL = "jdbc:sqlserver://localhost:1433;databaseName=University";
+    public static String dbURL = "jdbc:mysql://localhost:3306/test";
+    public static String dbUser = "";
+    public static String dbPass = "";
+
+}
